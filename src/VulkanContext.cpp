aa4cd5d0a8807a3dda60b6134ed8350b569058ad
Yukari-dev <gnail102g@gmail.com>
2026-05-31 00:22:23 +0100

    Adding Validation Layers.

--- src/VulkanContext.cpp
+++ src/VulkanContext.cpp
@@ -1,8 +1,18 @@
 #include "VulkanContext.hpp"
 #include <stdexcept>
 #include <iostream>
+#include <cstring>
 
 VulkanContext::VulkanContext(){
+    CreateInstance();
+}
+
+VulkanContext::~VulkanContext(){
+    if(m_Instance)
+        vkDestroyInstance(m_Instance, nullptr);
+}
+
+void VulkanContext::CreateInstance(){
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     appInfo.pApplicationName = "Voxely";
@@ -15,6 +25,19 @@ VulkanContext::VulkanContext(){
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     createInfo.pApplicationInfo = &appInfo;
 
+    if(m_EnableValidationLayers && !CheckValidationLayers()){
+        throw std::runtime_error("Validation Layers not available.");
+    }
+
+    std::cout << "Validation Layers are available.\n";
+
+    if(m_EnableValidationLayers){
+        createInfo.enabledLayerCount = static_cast<uint32_t>(m_ValidationLayers.size());
+        createInfo.ppEnabledLayerNames = m_ValidationLayers.data();
+    } else{
+        createInfo.enabledLayerCount = 0;
+    }
+
     if(vkCreateInstance(
         &createInfo,
         nullptr, &m_Instance
@@ -24,6 +47,30 @@ VulkanContext::VulkanContext(){
     std::cout << "Vulkan instance created!\n";
 }
 
-VulkanContext::~VulkanContext(){
+bool VulkanContext::CheckValidationLayers(){
+    uint32_t layerCount;
 
+    vkEnumerateInstanceLayerProperties(
+        &layerCount, nullptr
+    );
+
+    std::vector<VkLayerProperties> layers(layerCount);
+
+    vkEnumerateInstanceLayerProperties(
+        &layerCount,
+        layers.data()
+    );
+
+    for(const char *layerName : m_ValidationLayers){
+        bool found = false;
+        for(const auto &layer : layers){
+            if(strcmp(layerName, layer.layerName) == 0){
+                found = true;
+                break;
+            }
+        }
+        if(!found) return false;
+    }
+
+    return true;
 }
--- src/VulkanContext.hpp
+++ src/VulkanContext.hpp
@@ -1,10 +1,23 @@
 #pragma once
 #include <vulkan/vulkan.h>
+#include <vector>
 
 class VulkanContext{
 public:
     VulkanContext();
     ~VulkanContext();
 private:
+    void CreateInstance();
+    bool CheckValidationLayers();
     VkInstance m_Instance;
+
+    const std::vector<char const*> m_ValidationLayers = {
+        "VK_LAYER_KHRONOS_validation"
+    };
+
+    #ifdef NDEBUG
+        const bool m_EnableValidationLayers = false;
+    #else
+        const bool m_EnableValidationLayers = false;
+    #endif
 };
